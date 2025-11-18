80ce626f883de087ae1793d6a4d181a93c14973b
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2025-11-19 00:34:01 +0100

    popup with info about governmants

---
+++ scripts/popup.js
@@ -0,0 +1,9 @@
+
+function closePopup() {
+  const popup = document.querySelector('.popup');
+  popup.style.display = 'none';
+}
+function showPopup() {
+  const popup = document.querySelector('.popup');
+  popup.style.display = 'block';
+}
