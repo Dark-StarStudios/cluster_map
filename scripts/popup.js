6d98829ad623251b6fb419b5705a8193ca33ee92
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2025-11-19 01:11:10 +0100

    update css

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,15 @@
+// const popup = document.querySelector('.popup');
+
 
 function closePopup() {
   const popup = document.querySelector('.popup');
+  const viewport = document.getElementById("viewport");
   popup.style.display = 'none';
+  viewport.style.display = 'block';
 }
 function showPopup() {
   const popup = document.querySelector('.popup');
+  const viewport = document.getElementById("viewport");
+viewport.style.display = 'none';
   popup.style.display = 'block';
 }
